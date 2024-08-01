a2a6f030a32b1b70f719ae125977901d14e8b053
pinje0 <melvinaustinn@gmail.com>
2024-08-01 20:32:53 +0700

    2024年8月1日

---
+++ typescript/easy/35.searchInsert.ts
@@ -0,0 +1,12 @@
+function searchInsert(nums: number[], target: number): number {
+    if (nums.includes(target)) {
+        return nums.indexOf(target)
+    } else {
+        nums.push(target)
+        const sorted = nums.sort((a, b) => a - b)
+        return sorted.indexOf(target)
+    }
+}
+
+console.log(searchInsert([1, 3, 5, 6], 5)) // target = 5, output = 2 ~ true
+console.log(searchInsert([1, 3, 5, 6], 2)) // target = 2, output 1 ~ false
