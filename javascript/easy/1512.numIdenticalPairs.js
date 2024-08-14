21a23f48ab3131f50b16d582924a9f7705b61368
pinje0 <melvinaustinn@gmail.com>
2024-08-14 14:23:09 +0700

    2024年8月14日

---
+++ javascript/easy/1512.numIdenticalPairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function (nums) {
+    let count = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3])) // 4
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+console.log(numIdenticalPairs([1, 1, 1, 1])) // 6
+console.log(numIdenticalPairs([1, 2, 3])) // 0
