2bd1ddbb018d19cd1fc734281f1b1416854b2272
pinje0 <melvinaustinn@gmail.com>
2024-08-26 13:45:04 +0700

    2024年8月26日

---
+++ javascript/easy/2894.differenceOfSums.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function (n, m) {
+    let num1 = 0
+    let num2 = 0
+    for (let i = 1; i <= n; i++) {
+        if (i % m !== 0) {
+            num1 += i
+        } else {
+            num2 += i
+        }
+    }
+
+    return num1 - num2
+}
+
+// console.log(differenceOfSums(10, 3)) // 19
+// Input: n = 10, m = 3
+// Output: 19
+// Explanation: In the given example:
+// - Integers in the range [1, 10] that are not divisible by 3 are [1,2,4,5,7,8,10], num1 is the sum of those integers = 37.
+// - Integers in the range [1, 10] that are divisible by 3 are [3,6,9], num2 is the sum of those integers = 18.
+// We return 37 - 18 = 19 as the answer.
+
+console.log(differenceOfSums(5, 6)) // 15
+// Input: n = 5, m = 6
+// Output: 15
+// Explanation: In the given example:
+// - Integers in the range [1, 5] that are not divisible by 6 are [1,2,3,4,5], num1 is the sum of those integers = 15.
+// - Integers in the range [1, 5] that are divisible by 6 are [], num2 is the sum of those integers = 0.
+// We return 15 - 0 = 15 as the answer.
