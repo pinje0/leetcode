b76e5e343b364d9962cd8bd3de49bb5e1cbeda12
pinje0 <melvinaustinn@gmail.com>
2024-09-01 21:18:50 +0700

    2024年8月1日

---
+++ javascript/easy/344.reverseString.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    s.reverse()
+}
+
+console.log(reverseString(["h", "e", "l", "l", "o"]))
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
---
+++ typescript/easy/344.reverseString.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    s.reverse()
+}
+
+console.log(reverseString(["h", "e", "l", "l", "o"]))
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
