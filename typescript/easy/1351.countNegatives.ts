4cf622d826044a52ebc046442ee6544768ea790a
pinje <melvinaustinn@gmail.com>
2024-09-03 02:46:06 +0700

    2024年8月2日'

---
+++ typescript/easy/1351.countNegatives.ts
@@ -0,0 +1,23 @@
+function countNegatives(grid: number[][]): number {
+    let count: number = 0
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] < 0) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+console.log(
+    countNegatives([
+        [4, 3, 2, -1],
+        [3, 2, 1, -1],
+        [1, 1, -1, -2],
+        [-1, -1, -2, -3],
+    ])
+) // 8
+// Output: 8
+// Explanation: There are 8 negatives number in the matrix.
