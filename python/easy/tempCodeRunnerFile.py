ac8a1136eec42b897756b5ef35f98e8be765c116
pinje <melvinaustinn@gmail.com>
2024-10-05 15:38:09 +0700

    2024年10月5日

--- python/easy/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-# print(solution.countSegments(""))  # 0
-# print(solution.countSegments("" ""))  # 0
-# print(
-#     solution.countSegments(
-#         "Of all the gin joints in all the towns in all the world,   "
-#     )
-# )  # 13
