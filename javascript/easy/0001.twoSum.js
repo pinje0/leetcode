cc1ebaaaf4ece283196cd2552ec8cac469ae7d23
pinje <melvinaustinn@gmail.com>
2025-08-03 22:53:55 +0700

    2025年8月3日

--- javascript/easy/0001.twoSum.js
+++ javascript/easy/0001.twoSum.js
@@ -1,18 +1,34 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] == target - nums[i]) {
-                return [i, j]
-            }
-        }
-    }
-    return null
-}
 
-// console.log(twoSum([2, 7, 11, 15], 9)) // [0, 1]
-console.log(twoSum([3, 2, 4], 6)) // [0, 1]
+// 1
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[j] == target - nums[i]) {
+//         return [i, j];
+//       }
+//     }
+//   }
+//   return null;
+// };
+
+// 2
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i]);
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
