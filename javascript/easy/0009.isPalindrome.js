57dc962ce9dad4bb56bbba1ceac597e40fc4eebc
pinje <melvinaustinn@gmail.com>
2025-08-05 13:39:07 +0700

    2025年8月5日

--- javascript/easy/0009.isPalindrome.js
+++ javascript/easy/0009.isPalindrome.js
@@ -1,18 +1,39 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number} x
  * @return {boolean}
  */
+
+//* 1
+// var isPalindrome = function (x) {
+//   let v1 = x.toString();
+//   let v2 = '';
+
+//   for (let i = v1.length - 1; i >= 0; i--) {
+//     v2 += v1[i];
+//   }
+
+//   return v1 == v2 ? true : false;
+// };
+
+//*  2
 var isPalindrome = function (x) {
-    let v1 = x.toString()
-    let v2 = ""
+  if (x < 0) {
+    return false;
+  }
 
-    for (let i = v1.length - 1; i >= 0; i--) {
-        v2 += v1[i]
-    }
+  let toStr = x.toString();
+  let reversed = '';
+  for (let i = toStr.length - 1; i >= 0; i--) {
+    reversed += toStr[i];
+  }
 
-    return v1 == v2 ? true : false
-}
+  return toStr == reversed ? true : false;
+};
 
-// console.log(isPalindrome(121)) // true
-// console.log(isPalindrome(-121)) // false
-// console.log(isPalindrome(10)) // false
+//? Output
+console.log(isPalindrome(121)); // true
+console.log(isPalindrome(-121)); // false
+console.log(isPalindrome(10)); // false
