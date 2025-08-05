1ed73b688df0fdba902daf68e6a931240f410622
pinje <melvinaustinn@gmail.com>
2025-08-05 14:07:00 +0700

    2025年8月5日

--- javascript/easy/0035.searchInsert.js
+++ javascript/easy/0035.searchInsert.js
@@ -1,18 +1,43 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
+
+//* 1
+// var searchInsert = function (nums, target) {
+//   if (nums.includes(target)) {
+//     return nums.indexOf(target);
+//   } else {
+//     nums.push(target);
+//     const sorted = nums.sort((a, b) => a - b);
+
+//     return sorted.indexOf(target);
+//   }
+// };
+
+//* 2
 var searchInsert = function (nums, target) {
-    if (nums.includes(target)) {
-        return nums.indexOf(target)
-    } else {
-        nums.push(target)
-        const sorted = nums.sort((a, b) => a - b)
-
-        return sorted.indexOf(target)
+  if (nums.includes(target)) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] == target) {
+        return i;
+      }
     }
-}
+  } else {
+    const newNums = [...nums, target];
+    const sortedArr = newNums.sort((a, b) => a - b);
+    for (let i = 0; i < sortedArr.length; i++) {
+      if (sortedArr[i] == target) {
+        return i;
+      }
+    }
+  }
+};
 
-console.log(searchInsert([1, 3, 5, 6], 5)) // target = 5, output = 2 ~ true
-console.log(searchInsert([1, 3, 5, 6], 2)) // target = 2, output 1 ~ false
+//? Output
+console.log(searchInsert([1, 3, 5, 6], 5)); // target = 5, output = 2 (true condition)
+console.log(searchInsert([1, 3, 5, 6], 2)); // target = 2, output 1 (false condition
