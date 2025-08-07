b3b702e37fafc851a63ad85c93b1916e8c3f67d9
pinje <melvinaustinn@gmail.com>
2025-08-07 13:57:20 +0700

    2025年8月7日

--- javascript/easy/0121.maxProfit.js
+++ javascript/easy/0121.maxProfit.js
@@ -27,9 +27,9 @@
 
 //* 2
 
-//TODO console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
 //TODO Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 //TODO Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-//TODO console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+// console.log(maxProfit([7, 6, 4, 3, 1])); // 0
 //TODO Explanation: In this case, no transactions are done and the max profit = 0.
--- javascript/easy/0557.reverseWords.js
+++ javascript/easy/0557.reverseWords.js
@@ -1,23 +1,37 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {string} s
  * @return {string}
  */
+
+//* 1
+////var reverseWords = function (s) {
+////    let str = ""
+////
+////    for (let i = s.length - 1; i >= 0; i--) {
+////        str += s[i]
+////    }
+////
+////    return str.split(" ").reverse().join(" ")
+////}
+
 var reverseWords = function (s) {
-    let str = ""
+  const split = s.split('');
+  let reverse = '';
+  for (let i = split.length - 1; i >= 0; i--) {
+    reverse += split[i];
+  }
+  return reverse.split(' ').reverse().join(' ');
+};
 
-    for (let i = s.length - 1; i >= 0; i--) {
-        str += s[i]
-    }
+console.log(reverseWords("Let's take LeetCode contest"));
+//TODO Example 1:
+//TODO Input: s = "Let's take LeetCode contest"
+//TODO Output: "s'teL ekat edoCteeL tsetnoc"
 
-    return str.split(" ").reverse().join(" ")
-}
-
-console.log(reverseWords("Let's take LeetCode contest"))
-// Example 1:
-// Input: s = "Let's take LeetCode contest"
-// Output: "s'teL ekat edoCteeL tsetnoc"
-
-console.log(reverseWords("Mr Ding"))
-// Example 2:
-// Input: s = "Mr Ding"
-// Output: "rM gniD"
+// console.log(reverseWords('Mr Ding'));
+//TODO Example 2:
+//TODO Input: s = "Mr Ding"
+//TODO Output: "rM gniD"
