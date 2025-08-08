163d54ecfa10bf4ce57280da7c556082855e224e
pinje <melvinaustinn@gmail.com>
2025-08-08 13:51:44 +0700

    2025年8月8日

--- javascript/easy/0704.binarySearch.js
+++ javascript/easy/0704.binarySearch.js
@@ -1,10 +1,21 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
-var search = function (nums, target) {
-    return nums.indexOf(target)
-}
+//// var search = function (nums, target) {
+////    return nums.indexOf(target)
+//// };
 
-console.log(search([-1, 0, 3, 5, 9, 12], 9)) // 4
+var search = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == target) {
+      return i;
+    }
+  }
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
