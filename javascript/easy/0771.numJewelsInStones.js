b7bafd1b90f74f6471a0ef9dcc2a0bb239485601
pinje <melvinaustinn@gmail.com>
2025-08-10 23:06:13 +0700

    2025年8月10日

--- javascript/easy/0771.numJewelsInStones.js
+++ javascript/easy/0771.numJewelsInStones.js
@@ -1,18 +1,39 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {string} jewels
  * @param {string} stones
  * @return {number}
  */
-var numJewelsInStones = function (jewels, stones) {
-    let counter = 0
-    for (let i = 0; i < stones.length; i++) {
-        if (jewels.includes(stones[i])) {
-            counter++
-        }
-    }
-    return counter
-}
 
-console.log(numJewelsInStones("aA", "aAAbbbb")) // 3
-console.log(numJewelsInStones("z", "ZZ")) // 0
+//* 1
+///// var numJewelsInStones = function (jewels, stones) {
+/////   let counter = 0;
+/////   for (let i = 0; i < stones.length; i++) {
+/////     if (jewels.includes(stones[i])) {
+/////       counter++;
+/////     }
+/////   }
+/////   return counter;
+///// };
+
+//* 2
+var numJewelsInStones = function (jewels, stones) {
+  let sum = 0;
+
+  for (let i = 0; i < stones.length; i++) {
+    if (jewels.includes(stones[i])) {
+      sum++;
+    }
+  }
+  return sum;
+};
+
+//TODO You're given strings `jewels` representing the types of stones that are jewels, and `stones` representing the stones you have.
+//TODO Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+//TODO Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+console.log(numJewelsInStones('aA', 'aAAbbbb')); // 3
+// console.log(numJewelsInStones('z', 'ZZ')); // 0
 // Letters are case sensitive, so "a" is considered a different type of stone from "A".
--- javascript/easy/0832.flipAndInvertImage.js
+++ javascript/easy/0832.flipAndInvertImage.js
@@ -1,39 +1,54 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number[][]} image
  * @return {number[][]}
  */
-var flipAndInvertImage = function (image) {
-  let invertRow = image.map((row) => row.reverse());
 
-  for (let i = 0; i < invertRow.length; i++) {
-    for (let j = 0; j < invertRow[i].length; j++) {
-      if (invertRow[i][j] == 0) {
-        invertRow[i][j] = 1;
+//* 1
+//// var flipAndInvertImage = function (image) {
+////   let invertRow = image.map((row) => row.reverse());
+
+////   for (let i = 0; i < invertRow.length; i++) {
+////     for (let j = 0; j < invertRow[i].length; j++) {
+////       if (invertRow[i][j] == 0) {
+////         invertRow[i][j] = 1;
+////       } else {
+////         invertRow[i][j] = 0;
+////       }
+////     }
+////   }
+
+////   return invertRow;
+//// };
+
+//* 2
+var flipAndInvertImage = function (image) {
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+    for (let j = 0; j < image[i].length; j++) {
+      if (image[i][j]) {
+        image[i][j] = 0;
       } else {
-        invertRow[i][j] = 0;
+        image[i][j] = 1;
       }
     }
   }
-
-  return invertRow;
+  return image;
 };
 
-console.log(
-  flipAndInvertImage([
-    [1, 1, 0],
-    [1, 0, 1],
-    [0, 0, 0],
-  ])
-); // [[1,0,0],[0,1,0],[1,1,1]]
+//TODO Given an `n x n` binary matrix `image`, flip the image horizontally, then invert it, and return the resulting image.
+//TODO To flip an image horizontally means that each row of the image is reversed.
+//TODO
+//TODO - For example, flipping [1,1,0] horizontally results in [0,1,1].
+//TODO To invert an image means that each `0` is replaced by `1`, and each `1` is replaced by `0`.
+//TODO - For example, inverting `[0,1,1]` results in `[1,0,0]`.
 
+// prettier-ignore
+console.log(flipAndInvertImage([[1, 1, 0],[1, 0, 1],[0, 0, 0],])); // [[1,0,0],[0,1,0],[1,1,1]]
 // Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
 // Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
 
-console.log(
-  flipAndInvertImage([
-    [1, 1, 0, 0],
-    [1, 0, 0, 1],
-    [0, 1, 1, 1],
-    [1, 0, 1, 0],
-  ]) // [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
-);
+// prettier-ignore
+console.log(flipAndInvertImage([ [1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0],])); // [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
