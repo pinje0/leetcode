b7a2f4aeabacb4d2e142df58a05507c67d135947
pinje <melvinaustinn@gmail.com>
2025-08-11 15:51:35 +0700

    2025年8月11日

--- javascript/easy/1108.defangIPaddr.js
+++ javascript/easy/1108.defangIPaddr.js
@@ -1,10 +1,29 @@
+//  @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {string} address
  * @return {string}
  */
-var defangIPaddr = function (address) {
-    return address.replaceAll(".", "[.]")
-}
 
-console.log(defangIPaddr("1.1.1.1")) // "1[.]1[.]1[.]1"
-console.log(defangIPaddr("255.100.50.0")) // "255[.]100[.]50[.]0"
+//* 1
+// var defangIPaddr = function (address) {
+//     return address.replaceAll(".", "[.]")
+// }
+
+//* 2
+var defangIPaddr = function (address) {
+  let res = '';
+  for (let i = 0; i < address.length; i++) {
+    console.log(address[i]);
+    if (address[i] === '.') {
+      res += '[.]';
+    } else {
+      res += address[i];
+    }
+  }
+  return res;
+};
+
+console.log(defangIPaddr('1.1.1.1')); // "1[.]1[.]1[.]1"
+// console.log(defangIPaddr('255.100.50.0')); // "255[.]100[.]50[.]0"
