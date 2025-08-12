392cac5f3531c3d3bc83e5ac2837ce1b8c109b92
pinje <melvinaustinn@gmail.com>
2025-08-12 22:07:22 +0700

    2025年8月12日

--- javascript/easy/1281.subtractProductAndSum.js
+++ javascript/easy/1281.subtractProductAndSum.js
@@ -1,24 +1,46 @@
+// @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number} n
  * @return {number}
  */
+
+//* 1
+////var subtractProductAndSum = function (n) {
+////  let m = String(n)
+////    .split('')
+////    .reduce((acc, curr) => acc * curr);
+////
+////  let s = String(n)
+////    .split('')
+////    .reduce((acc, curr) => Number(acc) + Number(curr));
+////
+////  return m - s;
+////};
+
+//* 2
 var subtractProductAndSum = function (n) {
-    let m = String(n)
-        .split("")
-        .reduce((acc, curr) => acc * curr)
+  const nums = [];
 
-    let s = String(n)
-        .split("")
-        .reduce((acc, curr) => Number(acc) + Number(curr))
+  for (let i = 0; i < String(n).length; i++) {
+    nums.push(Number(String(n)[i]));
+  }
 
-    return m - s
-}
+  // Substract
+  const subtract = nums.reduce((acc, curr) => acc * curr);
 
-console.log(subtractProductAndSum(234)) // 15
+  // Sums
+  const sum = nums.reduce((acc, curr) => acc + curr);
+
+  return subtract - sum;
+};
+
+console.log(subtractProductAndSum(234)); // 15
 // Output: 15
 // Explanation:
 // Product of digits = 2 * 3 * 4 = 24
 // Sum of digits = 2 + 3 + 4 = 9
 // Result = 24 - 9 = 15
 
-console.log(subtractProductAndSum(4421))
+// console.log(subtractProductAndSum(4421))
---
+++ note.md
@@ -0,0 +1,13 @@
+1. Tracking
+
+```js
+// @tracking
+//! Re-Solved Times: 2
+```
+
+2. Fix console.log multiple line
+   Add this on top of the multiple line code/console
+
+```prettier
+// prettier-ignore
+```
