8bf0cf0ef9c21f1db0836f25869fbbc17d265ee6
pinje <melvinaustinn@gmail.com>
2025-08-17 15:00:44 +0700

    2025年8月17日

--- javascript/easy/1351.countNegatives.js
+++ javascript/easy/1351.countNegatives.js
@@ -1,27 +1,42 @@
+// @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number[][]} grid
  * @return {number}
  */
+
+//* 1
+////var countNegatives = function (grid) {
+////  let count = 0;
+////
+////  for (let i = 0; i < grid.length; i++) {
+////    for (let j = 0; j < grid[i].length; j++) {
+////      if (grid[i][j] < 0) {
+////        count++;
+////      }
+////    }
+////  }
+////  return count;
+////
+//// };
+
+//* 2
 var countNegatives = function (grid) {
-    let count = 0
+  let counter = 0;
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] < 0) {
-                count++
-            }
-        }
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] < 0) {
+        counter++;
+      }
     }
-    return count
-}
+  }
 
-console.log(
-    countNegatives([
-        [4, 3, 2, -1],
-        [3, 2, 1, -1],
-        [1, 1, -1, -2],
-        [-1, -1, -2, -3],
-    ])
-) // 8
+  return counter;
+};
+
+// prettier-ignore
+console.log(countNegatives([[4, 3, 2, -1],[3, 2, 1, -1],[1, 1, -1, -2],[-1, -1, -2, -3],])) // 8
 // Output: 8
 // Explanation: There are 8 negatives number in the matrix.
