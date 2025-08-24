a09c0dbba4a08a958d417fd8982712fb69248fd5
pinje <melvinaustinn@gmail.com>
2025-08-24 23:13:16 +0700

    2025年8月24日

--- javascript/easy/1662.arrayStringsAreEqual.js
+++ javascript/easy/1662.arrayStringsAreEqual.js
@@ -1,13 +1,33 @@
+// @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {string[]} word1
  * @param {string[]} word2
  * @return {boolean}
  */
-var arrayStringsAreEqual = function (word1, word2) {
-    return word1.join("") === word2.join("") ? true : false
-}
 
-console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"]))
+//* 1
+////var arrayStringsAreEqual = function (word1, word2) {
+////  return word1.join('') === word2.join('') ? true : false;
+////};
+
+//* 2
+var arrayStringsAreEqual = function (word1, word2) {
+  let w1 = '';
+  let w2 = '';
+  for (let i = 0; i < word1.length; i++) {
+    w1 += word1[i];
+  }
+
+  for (let i = 0; i < word2.length; i++) {
+    w2 += word2[i];
+  }
+
+  return w1 === w2 ? true : false;
+};
+
+console.log(arrayStringsAreEqual(['ab', 'c'], ['a', 'bc']));
 
 // Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
 // Output: true
@@ -16,5 +36,5 @@ console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"]))
 // word2 represents string "a" + "bc" -> "abc"
 // The strings are the same, so return true.
 
-console.log(arrayStringsAreEqual(["a", "cb"], ["ab", "c"])) // false
-console.log(arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"]))
+console.log(arrayStringsAreEqual(['a', 'cb'], ['ab', 'c'])); // false
+console.log(arrayStringsAreEqual(['abc', 'd', 'defg'], ['abcddefg']));
