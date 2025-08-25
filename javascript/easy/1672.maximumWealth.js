3850c047f8df7c4c5f1dcba800406c67c8bf2938
pinje <melvinaustinn@gmail.com>
2025-08-25 23:21:43 +0700

    2025年8月25日

--- javascript/easy/1672.maximumWealth.js
+++ javascript/easy/1672.maximumWealth.js
@@ -1,43 +1,48 @@
+// @tracking
+//! Re-Solved Times: 2
+
 /**
  * @param {number[][]} accounts
  * @return {number}
  */
-var maximumWealth = function (accounts) {
-  let max = accounts[0].reduce((a, b) => a + b);
 
-  for (let i = 1; i < accounts.length; i++) {
-    if (max < accounts[i].reduce((a, b) => a + b)) {
-      max = accounts[i].reduce((a, b) => a + b);
+//* 1
+////var maximumWealth = function (accounts) {
+////  let max = accounts[0].reduce((a, b) => a + b);
+////
+////  for (let i = 1; i < accounts.length; i++) {
+////    if (max < accounts[i].reduce((a, b) => a + b)) {
+////      max = accounts[i].reduce((a, b) => a + b);
+////    }
+////  }
+////
+////  return max;
+////};
+
+//* 2
+var maximumWealth = function (accounts) {
+  let max = 0;
+
+  for (let i = 0; i < accounts.length; i++) {
+    let currentWealth = accounts[i].reduce((a, b) => a + b);
+    if (max < currentWealth) {
+      max = currentWealth;
     }
   }
 
   return max;
 };
 
-console.log(
-  maximumWealth([
-    [1, 5],
-    [7, 3],
-    [3, 5],
-  ])
-); // 10
+// prettier-ignore
+console.log(maximumWealth([[1, 2, 3],[3, 2, 1],])); // 6
 
-console.log(
-  maximumWealth([
-    [1, 2, 3],
-    [3, 2, 1],
-  ])
-); // 6
+//TODO Explanation:
+//TODO 1st customer has wealth = 1 + 2 + 3 = 6
+//TODO 2nd customer has wealth = 3 + 2 + 1 = 6
+//TODO Both customers are considered the richest with a wealth of 6 each, so return 6.
 
-console.log(
-  maximumWealth([
-    [2, 8, 7],
-    [7, 1, 3],
-    [1, 9, 5],
-  ])
-);
+// prettier-ignore
+console.log(maximumWealth([[1, 5],[7, 3],[3, 5],])); // 10
 
-// Explanation:
-// 1st customer has wealth = 1 + 2 + 3 = 6
-// 2nd customer has wealth = 3 + 2 + 1 = 6
-// Both customers are considered the richest with a wealth of 6 each, so return 6.
+// prettier-ignore
+console.log(maximumWealth([[2, 8, 7],[7, 1, 3],[1, 9, 5],]));
